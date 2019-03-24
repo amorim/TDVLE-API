300c57d486c3d6bd2ad70083ee46bfa078eb3e87
Lucas Amorim <amorimlucas1998@gmail.com>
2019-03-24 14:30:06 -0300

    deploy

---
+++ src/test/groovy/tdn/api/FeatureControllerSpec.groovy
@@ -0,0 +1,18 @@
+package tdn.api
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class FeatureControllerSpec extends Specification implements ControllerUnitTest<FeatureController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
---
+++ src/test/groovy/tdn/api/FeaturesSpec.groovy
@@ -0,0 +1,18 @@
+package tdn.api
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class FeaturesSpec extends Specification implements DomainUnitTest<Features> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
